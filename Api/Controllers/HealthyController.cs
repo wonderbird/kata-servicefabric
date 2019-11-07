5757294d79853f1e8b04f458838c22ebdcce8d07
Stefan Boos <kontakt@boos.systems>
2019-11-07 05:55:50 +0100

    Add project files.

---
+++ Api/Controllers/HealthyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthyController : ControllerBase
+    {
+        private readonly ILogger<HealthyController> _logger;
+
+        public HealthyController(ILogger<HealthyController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            return new {message = "OK"};
+        }
+    }
+}
